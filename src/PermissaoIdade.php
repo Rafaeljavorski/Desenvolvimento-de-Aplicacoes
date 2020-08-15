0375f84c4df8aecb82fbc8ce3919e53216c6b5f9
Rafaeljavorski <rafaeljj12@gmail.comm>
2020-08-15 17:51:52 -0300

    teste

---
+++ src/PermissaoIdade.php
@@ -0,0 +1,15 @@
+// permissao de entrada
+
+<?php
+  $idade = 0;
+
+$idade = readline("informe sua idade!!! \n");
+
+  if($idade <=18 ){
+    echo "Entrada autorizada!";
+  }
+  else{
+  echo "Entrada Proibida";
+
+  }
+?>
