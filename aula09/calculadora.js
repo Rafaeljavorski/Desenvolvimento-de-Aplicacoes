8e142800a118ca6e3ed75e6f788902ef835e8bc9
Rafael <69019316+Rafaeljavorski@users.noreply.github.com>
2020-10-18 14:13:18 -0300

    Calculadora JS

---
+++ aula09/calculadora.js
@@ -0,0 +1,18 @@
+const nun1 = document.getElementById('num1');
+const nun1 = document.getElementById('num2');
+const nun1 = document.getElementById('num3');
+const nun1 = document.getElementById('num4');
+const nun1 = document.getElementById('num5');
+const nun1 = document.getElementById('num6');
+const nun1 = document.getElementById('num7');
+const nun1 = document.getElementById('num8');
+const nun1 = document.getElementById('num9');
+const nun1 = document.getElementById('num0');
+
+function somar(){
+    const n1 = parseInt(num1.value);
+    const n2 = parseInt(num2.value);
+    const r = n1 + n2;
+    resultado.value = r;
+
+}
